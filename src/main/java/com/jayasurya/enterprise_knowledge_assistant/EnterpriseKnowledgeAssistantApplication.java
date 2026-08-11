61ccb8a2158d5fa27bbca5207c31ba005ff76457
jayasurya <jayasuryamathavan@gmail.com>
2026-08-11 18:32:47 +0530

    Initial Project Setup

---
+++ src/main/java/com/jayasurya/enterprise_knowledge_assistant/EnterpriseKnowledgeAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.jayasurya.enterprise_knowledge_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseKnowledgeAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseKnowledgeAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayasurya/enterprise_knowledge_assistant/EnterpriseKnowledgeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayasurya.enterprise_knowledge_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseKnowledgeAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
